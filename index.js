8586a4bba93e2076be856a48e037c518fead51b9
cami98735264 <cr7montealegre2010@gmail.com>
2024-01-01 13:38:32 -0500

    Minor changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.on('messageCreate', async (message) => {
             };
             let negativeReactions = 0;
             let positiveReactions = 0;
-            const collector = msg.createReactionCollector({ filter: collectorFilter, time: 10000 });
+            const collector = msg.createReactionCollector({ filter: collectorFilter, time: 50000 });
             collector.on('collect', (reaction, user) => {
                 if(reaction.emoji.name === '✅') {
                     positiveReactions++;
